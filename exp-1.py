12cbfd937994421b5a416581051c26ca8a6a3b49
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-01-22 08:21:56 +0530

    done

--- exp-1.py
+++ exp-1.py
@@ -1,7 +1,6 @@
 # Experiment 1: Python Basics
-# 1. Read a number from the user
 num = int(input("Enter a number: "))
-# 2. Check whether the number is positive, negative, or zero
+
 if num > 0:
     print("The number is Positive")
 elif num < 0:
@@ -11,14 +10,10 @@ else:
 
 print("\n--- Student Details ---")
 
-
-# 3. Create variables for student details
 student_id = 1024              # int
 student_name = "Om Prakash"    # str
 marks = 92.5                   # float
 attendance_status = True       # bool
-
-# 4. Display value and data type of each variable
 print("Student ID:", student_id, "| Data Type:", type(student_id))
 print("Student Name:", student_name, "| Data Type:", type(student_name))
 print("Marks:", marks, "| Data Type:", type(marks))
@@ -26,7 +21,6 @@ print("Attendance Status:", attendance_status, "| Data Type:", type(attendance_s
 
 print("\n--- Arithmetic Operations ---")
 
-# 5. Arithmetic operations
 a = 20
 b = 5
 
--- lab-3.py
+++
@@ -1,5 +0,0 @@
-square=lambda x:x*x
-print(square(5))
-
-area=lambda l,b:l*b
-print(area(5,6))
