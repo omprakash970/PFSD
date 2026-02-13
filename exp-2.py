24ccee4ba3c5bcb0dba463c81536a886f6c1464f
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-02-13 23:18:42 +0530

    done

--- exp-2.py
+++ exp-2.py
@@ -1,38 +1,23 @@
-# Experiment 2: Simple Calculator
-
 try:
-    # 1. Read two numbers from the user
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-
-    # 2. Display menu
-    print("\nSelect Operation:")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-
-    # 3. User choice
-    choice = int(input("Enter your choice (1-4): "))
-
-    # 4. Perform operation
-    if choice == 1:
-        print("Result:", num1 + num2)
-
-    elif choice == 2:
-        print("Result:", num1 - num2)
-
-    elif choice == 3:
-        print("Result:", num1 * num2)
-
-    elif choice == 4:
-        if num2 == 0:
+    a=float(input("Enter the number"))
+    b=float(input("Enter the number"))
+    print("\nSelect Operation")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    choice=int(input("Enter the choice"))
+    if choice==1:
+        print("Result: ", a+b)
+    if choice==2:
+        print("Result:", a-b)
+    if choice==3:
+        print("Result: ", a*b)
+    elif choice ==4:
+        if b==0:
             print("Error: Division by zero is not allowed")
         else:
-            print("Result:", num1 / num2)
-
-    else:
-        print("Invalid choice! Please select between 1 and 4.")
-
+             print("Result:", a / b)
 except ValueError:
-    print("Error: Please enter valid numeric inputs.")
+    print("Error: value input!! please enter the number numbers only ")
+
