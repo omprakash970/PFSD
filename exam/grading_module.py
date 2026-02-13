890a20d46f831c08c86a5ae9bc4fcadb9bcdff34
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-02-13 16:40:39 +0530

    done

---
+++ exam/grading_module.py
@@ -0,0 +1,11 @@
+def calculate_grade(marks):
+    if marks>=90:
+        return 'A'
+    elif marks>=80:
+        return 'B'
+    elif marks>=70:
+        return 'C'
+    elif marks>=60:
+        return 'D'
+    else:
+        return 'F' 
