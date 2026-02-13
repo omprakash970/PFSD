b25526bc34bcb17d62dfe3284a08d6351e18afa8
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-02-13 22:54:01 +0530

    done

--- exam/ultimate.py
+++ exam/ultimate.py
@@ -1,25 +1,23 @@
-def write_attendance():
+def write_attendence():
     try:
-        n=int(input("Enter the number of students:"))
+        n=int(input("Enter the no.of students"))
         if n<=0:
             raise ValueError
-        with open("attendance.txt", "w") as file: 
+        with open("attendance.txt", "w") as file:
             for i in range(n):
-                name=input(f"Enter the name: \n")
-                status=input(f"Enter the attendance status(Present/Absent)\n")
-                file.write(f"{name}:{status}\n")
+                name=input(f"Enter the name of the student")
+                status=input(f"Present or Absent(P/A)")
+                file.write(f"{name}-{status}")
     except ValueError:
-
-            print("Invalid input. Please enter a positive integer for the number of students.")
+        print("Enter some +ve number")
     except Exception as e:
-            print(f"An error occurred: {e}")
-def read_attendance():
-    try:
-        with open("attendance.txt", "r") as file:
-            print("Attendance List:")
+        print(f"An error occurred: {e}")
+def read_attendence():
+    try: 
+        with open("attandance.txt", "r")as file:
             for line in file: 
                 print(line.strip())
     except FileNotFoundError:
-        print("Attendance file not found. Please record attendance first.")   
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        print("404 error not found")
+    except Exception as e: 
+        print(f"An error occurred : {e}")
